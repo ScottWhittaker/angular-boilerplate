bb3538f61dc24799e785607d48663644b1247e2a
Scott Whittaker <sw.fiftyfathom@gmail.com>
2014-11-10 14:39:28 +0000

    default gulp task creates a debug build and injects bower dependencies into index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,34 @@
+/*
+    https://github.com/gulpjs/gulp/
+ */
 var gulp = require('gulp');
 
-gulp.task('default', function () {
-   console.log('default gulp task');
+/*
+    https://github.com/klei/gulp-inject
+ */
+var inject = require('gulp-inject');
+
+/*
+    https://github.com/ck86/main-bower-files
+ */
+var mainBowerFiles = require('main-bower-files');
+
+/*
+    The destination for debug build
+ */
+var DEBUG_DEST = './debug';
+
+gulp.task('debug', function () {
+    var bowerFiles = gulp.src(mainBowerFiles(), {read: false});
+    return gulp.src('./src/index.html')
+        // The name option passed to inject is the string used to define the placeholder in index.html where the script
+        // tags will be injected e.g.
+        // <!-- bower:js -->
+        // bower files injected here
+        // <!-- endinject -->
+        .pipe(inject(bowerFiles, {name: 'bower'}))
+        .pipe(gulp.dest(DEBUG_DEST));
 });
+
+
+gulp.task('default', ['debug']);
