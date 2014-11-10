05cfed92b13062a55f7dc4bfceecd83bda5102a3
Scott Whittaker <sw.fiftyfathom@gmail.com>
2014-11-10 15:05:09 +0000

    adding clean task to delete debug directory

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,16 @@ var inject = require('gulp-inject');
 var mainBowerFiles = require('main-bower-files');
 
 /*
+    https://github.com/sindresorhus/del
+ */
+var del = require('del');
+
+/*
     The destination for debug build
  */
 var DEBUG_DEST = './debug';
 
-gulp.task('debug', function () {
+gulp.task('debug', ['clean'], function () {
     var bowerFiles = gulp.src(mainBowerFiles(), {read: false});
     return gulp.src('./src/index.html')
         // The name option passed to inject is the string used to define the placeholder in index.html where the script
@@ -30,5 +35,8 @@ gulp.task('debug', function () {
         .pipe(gulp.dest(DEBUG_DEST));
 });
 
+gulp.task('clean', function (cb) {
+    del([DEBUG_DEST], cb);
+});
 
 gulp.task('default', ['debug']);
